b6e06d1e497dfd909e3c88918d806009d46645c4
LaCapitainerie <hugo.antreassian@gmail.com>
2026-02-20 10:27:32 +0100

    Working Test battery for isFriend

---
+++ __tests__/api/friend/is-friend.test.ts
@@ -0,0 +1,71 @@
+import { describe, it, expect } from "vitest";
+import { socialNetwork } from "../../../src/index.ts";
+import { socialNetworkClient } from "../../../src/client.ts";
+import { getTestInstance } from "better-auth/test";
+
+describe("API - Is Friend", async () => {
+  const { auth, signInWithTestUser } = await getTestInstance({
+    plugins: [socialNetwork({
+      allowSelfFriendRequest: true,
+    })],
+  }, {
+    clientOptions: {
+      plugins: [socialNetworkClient()],
+    },
+  });
+
+  const { runWithUser, user } = await signInWithTestUser();
+  await runWithUser(async (headers) => {
+
+    it("should return false if user does not exist", async () => {
+      const response = await auth.api.isFriend({
+        query: {
+          friendId: 'id-that-does-not-exist',
+        },
+        headers,
+        asResponse: true,
+      });
+      const body = await response.json();
+      expect(body.isFriend).toBe(false);
+    });
+
+    it("should return false if the user is not a friend", async () => {
+      const response = await auth.api.isFriend({
+        query: {
+          friendId: user.id,
+        },
+        headers,
+        asResponse: true,
+      });
+      const body = await response.json();
+      expect(body.isFriend).toBe(false);
+    });
+
+    it("should return true if the user is a friend", async () => {
+
+      const { friendRequest } = await auth.api.sendFriendRequest({
+        body: {
+          receiverId: user.id,
+        },
+        headers,
+      });
+
+      await auth.api.acceptFriendRequest({
+        body: {
+          requestId: friendRequest.id,
+        },
+        headers,
+      });
+
+      const response = await auth.api.isFriend({
+        query: {
+          friendId: friendRequest.receiverId,
+        },
+        headers,
+        asResponse: true,
+      });
+      const body = await response.json();
+      expect(body.isFriend).toBe(true);
+    });
+  });
+});
